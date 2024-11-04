c3c06989d6461f118651a4f4ed53ff23fb95f240
Dariofelix84 <dariofa69@gmail.com>
2024-11-04 17:09:02 -0300

    Imprementação da pesquisa avançada ao BD da TelaOS

--- TelaCliente.java
+++ TelaCliente.java
@@ -43,7 +43,7 @@ public class TelaCliente extends javax.swing.JInternalFrame {
                 }
             }
 
-        } catch (SQLException e) {
+        } catch (SQLException e){
             JOptionPane.showMessageDialog(null, e);
         }
     }
